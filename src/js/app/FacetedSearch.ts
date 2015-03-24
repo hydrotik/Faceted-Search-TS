4d11696c918c1355d7b7f32486fd40c4bbdeb0a5
Donovan Adams <donovan@hydrotik.com>
2015-03-24 18:41:03 -0400

    started porting facetedsearch.js to TS

---
+++ src/js/app/FacetedSearch.ts
@@ -0,0 +1,57 @@
+
+
+module app {
+
+    export class FacetedSearch {
+
+        private settings: Object;
+
+        private defaults:Object;
+
+        /**
+        *   Application Facade
+        *   
+        *   @param {Object} search settings
+        */
+        constructor(settings: Object) {
+            this.settings = settings;
+
+            this.log('Initializing FacetedSearch');
+
+            this.defaults = {
+              items              : [{a:2,b:1,c:2},{a:2,b:2,c:1},{a:1,b:1,c:1},{a:3,b:3,c:1}],
+              facets             : {'a': 'Title A', 'b': 'Title B', 'c': 'Title C'},
+              resultSelector     : '#results',
+              facetSelector      : '#facets',
+              facetContainer     : '<div class=facetsearch id=<%= id %> ></div>',
+              facetTitleTemplate : '<h3 class=facettitle><%= title %></h3>',
+              facetListContainer : '<div class=facetlist></div>',
+              listItemTemplate   : '<div class=facetitem id="<%= id %>"><%= name %> <span class=facetitemcount>(<%= count %>)</span></div>',
+              bottomContainer    : '<div class=bottomline></div>',
+              orderByTemplate    : '<div class=orderby><span class="orderby-title">Sort by: </span><ul><% _.each(options, function(value, key) { %>'+
+                                   '<li class=orderbyitem id=orderby_<%= key %>>'+
+                                   '<%= value %> </li> <% }); %></ul></div>',
+              countTemplate      : '<div class=facettotalcount><%= count %> Results</div>',
+              deselectTemplate   : '<div class=deselectstartover>Deselect all filters</div>',
+              resultTemplate     : '<div class=facetresultbox><%= name %></div>',
+              noResults          : '<div class=results>Sorry, but no items match these criteria</div>',
+              orderByOptions     : {'a': 'by A', 'b': 'by B', 'RANDOM': 'by random'},
+              state              : {
+                                     orderBy : false,
+                                     filters : {}
+                                   },
+              showMoreTemplate   : '<a id=showmorebutton>Show more</a>',
+              enablePagination   : true,
+              paginationCount    : 20
+            }
+        }
+
+        private log(message: string): void {
+            console.log(message);
+        }
+
+        init(message:string): void {
+            this.log(message);
+        }
+    }
+}
